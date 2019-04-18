943255f072fe749188d2dde28ddf3838870c2bdf
jialin.fu <jialin.fu@downjoy.com>
2019-04-18 15:47:16 +0800

    父类

---
+++ demo_parent/src/main/java/com/example/cloud/sample_parent/SampleParentApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloud.sample_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleParentApplication.class, args);
+	}
+
+}
